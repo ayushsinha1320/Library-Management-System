c41a44742f957558bc1556fa84436be330ff9a80
Ayush Sinha <ayushs@testvagrant.com>
2024-11-04 17:58:24 +0530

    fix: Updated the borrow book collection name

--- src/LMS/Member.java
+++ src/LMS/Member.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Member {
     private final String name;
     private final String member_id;
-    private List<Book> Books= new ArrayList<Book>();
+    private List<Book> borrowed_books= new ArrayList<Book>();
 
     public Member() {
         name = "NO NAME PROVIDED";
@@ -16,6 +16,6 @@ public class Member {
     public Member(String name, String member_id, List<Book> books) {
         this.name = name;
         this.member_id = member_id;
-        Books = books;
+        borrowed_books = books;
     }
 }
